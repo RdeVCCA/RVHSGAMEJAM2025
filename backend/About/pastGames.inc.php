e9944e902db0e37d4a431a8784aeda1a5208ad89
h-isthebestletter <hmmyes123454321@gmail.com>
2024-10-08 20:54:23 +0800

    fix broken thumbnails (hopefully)

--- backend/About/pastGames.inc.php
+++ backend/About/pastGames.inc.php
@@ -1,9 +1,12 @@
 <?php
 require "backend/Defaults/connect.php";
 function convertToCamelCase($string) {
-    // Replace any non-letter and non-digit character with a space
-    $string = preg_replace('/[^a-zA-Z0-9]/', ' ', $string);
+    // Removes every character that isn't a letter, number or space
+    $string = preg_replace('/[^a-zA-Z0-9 ]/', '', $string);
 
+    // Make the whole string lowercase
+    $string = mb_strtolower($string);
+    
     // Capitalize the first letter of each word and then remove spaces
     $string = str_replace(' ', '', ucwords($string));
 
@@ -14,6 +17,7 @@ function convertToCamelCase($string) {
 }
 
 function convertToFileLink($name, $year, $type) {
+    // `type` is one of 3 values, specify type to get different files:
     // 0 = video, 1 = image, 2 = logo
     $mimeTypes = [
         "mp4",
@@ -43,16 +47,15 @@ while ($row = mysqli_fetch_assoc($result)) {
         "description" => $row['description'],
         "creators" => $row['creators']
     ];
-    $logo = convertToFileLink($row['name'], $year, 2);
+    $logo = convertToFileLink($row['name'], $year, 2); // get logo
     if (file_exists($logo)){
         $new['logo'] = $logo;
     }
-    $video = convertToFileLink($row['name'], $year, 0);
+    $video = convertToFileLink($row['name'], $year, 0); // get video
     if (file_exists($video)){
         $new['video'] = $video;
     }
-    
-    $thumbnail = convertToFileLink($row['name'], $year, 1);
+    $thumbnail = convertToFileLink($row['name'], $year, 1); // get thumbnail
     if (file_exists($thumbnail)){
         $new['thumbnail'] = $thumbnail;
     }
